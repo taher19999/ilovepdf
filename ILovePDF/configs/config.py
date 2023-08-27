a83dae81f64dbc932f296bd48f58cd95ebd6ebeb
taher19999 <94671425+taher19999@users.noreply.github.com>
2023-08-27 08:00:34 -0700

    Update config.py

--- ILovePDF/configs/config.py
+++ ILovePDF/configs/config.py
@@ -84,7 +84,7 @@ class settings(object):
     DEFAULT_LANG = os.environ.get("DEFAULT_LANG", "eng")  # use small letters
 
     MULTI_LANG_SUP = (
-        True if os.environ.get("MULTI_LANG_SUP", "False") == "True" else False
+        True if os.environ.get("MULTI_LANG_SUP", "True") == "True" else False
     )
 
     REPORT = "https://t.me/ilovepdf_bot/33?comment=1000000000"
