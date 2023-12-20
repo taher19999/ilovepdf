909f1e0f4eed25b83a068fc73ff9c465a64f3615
taher19999 <94671425+taher19999@users.noreply.github.com>
2023-12-20 09:50:16 +0300

    Update handler.py

--- ILovePDF/plugins/dm/textToPdf/handler.py
+++ ILovePDF/plugins/dm/textToPdf/handler.py
@@ -151,10 +151,10 @@ async def text_to_pdf(bot, callbackQuery):
 
         pdf.set_title("NABIL A NAVAB")
         pdf.set_subject(
-            "pdf created using nabilanavab open source Telegram Pdf Bot\n\nContact Nabil A Navab: telegram.dog/nabilanavab ❤"
+            "rate bot  ❤"
         )
-        pdf.set_author("https://github.com/nabilanavab/ilovepdf")
-        pdf.set_producer("by nabilanavab@gmail.com")
+        pdf.set_author("https://telegramic.org/bot/i2pdfbot/")
+        pdf.set_producer("by taheralnoori19999@gmail.com")
 
         pdf.add_font("headFont", "", FONT[h_font], uni=True)
         pdf.set_font("headFont", "", size=30)
